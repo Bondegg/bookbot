f7ffb36c8660df5fed0338241e6e553aa1071544
Bondegg <harry_bond@hotmail.co.uk>
2025-10-26 18:50:48 +0000

    add function to sort a list of dictionaries (character, count) and arrange them from highest to lowest count. Then print formatted results

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import book_word_count, book_character_count
+from stats import book_word_count, book_character_count, sort_word_count
 
 def get_book_text(filepath):
     with open(filepath) as book:
@@ -9,7 +9,17 @@ def main():
     book_text = get_book_text(book_path)
     word_count = book_word_count(book_text)
     character_dict = book_character_count(book_text)
+    character_count_sorted = sort_word_count(character_dict)
     print(f"Found {word_count} total words")
-    print(character_dict)
+    
+    for entry in character_count_sorted:
+        char = entry["char"]
+        num = entry["num"]
+
+        if char.isalpha():
+            print(f"{char}: {num}")
+        else:
+            continue
+    
 
 main()
--- stats.py
+++ stats.py
@@ -4,6 +4,7 @@ def book_word_count(book_text):
 
 def book_character_count(book):
     character_count = {}
+
     for word in book:
         lower_word = word.lower()
         for letter in lower_word:
@@ -17,4 +18,19 @@ def book_character_count(book):
                 continue
 
     return character_count
+
+def sort_by(items):
+    return items["num"]
+
+def sort_word_count(characters_dict):
+    list_to_sort = []
+    for key, value in characters_dict.items():
+        temp_dict = {"char": key, "num": value}
+        list_to_sort.append(temp_dict)
+    
+    list_to_sort.sort(reverse=True, key=sort_by)
+
+    return list_to_sort
+
+    
 
