e16f72ce5c8ca15a8be729267853668ff33af595
Bondegg <harry_bond@hotmail.co.uk>
2025-10-26 19:04:14 +0000

    remove hard coded paths to test books, use sys.argv to look for specified path when ran through CLI

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from stats import book_word_count, book_character_count, sort_word_count
+import sys
 
 def get_book_text(filepath):
     with open(filepath) as book:
         return book.read()
     
 def main():
-    book_path = "books/frankenstein.txt"
+
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
+    book_path = sys.argv[1]
     book_text = get_book_text(book_path)
     word_count = book_word_count(book_text)
     character_dict = book_character_count(book_text)
